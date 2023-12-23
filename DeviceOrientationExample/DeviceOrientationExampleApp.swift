90ed37e0c1fb2eb388a4c0cfc8dc5d1d55fba8ef
Fatih Durmaz <fatih.durmaz@outlook.com>
2023-12-24 02:45:11 +0300

    Initial Commit

---
+++ DeviceOrientationExample/DeviceOrientationExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeviceOrientationExampleApp.swift
+//  DeviceOrientationExample
+//
+//  Created by Fatih Durmaz on 24.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct DeviceOrientationExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
