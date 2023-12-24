d33f0c1fd9a7b4a3695d4da683f7bf16a8a66d17
Fatih Durmaz <fatih.durmaz@outlook.com>
2023-12-25 01:48:07 +0300

    first commit

--- DeviceOrientationExample/ContentView.swift
+++ DeviceOrientationExample/ContentView.swift
@@ -6,16 +6,46 @@
 //
 
 import SwiftUI
+import Lottie
 
 struct ContentView: View {
+    
+    @Environment(\.horizontalSizeClass) var horizontalSizeClass
+    @Environment(\.verticalSizeClass) var verticalSizeClass
+    
+    
+    init(){
+        let apperance = UINavigationBarAppearance()
+        apperance.configureWithOpaqueBackground()
+        UINavigationBar.appearance().standardAppearance = apperance
+        UINavigationBar.appearance().scrollEdgeAppearance = apperance
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        GeometryReader { geo in
+            NavigationStack {
+                VStack {
+                    // iPad için cihaz yönü algılama geometry reader ile
+                    /*
+                     Text("Ekranın Konumu: \(geo.size.width > geo.size.height ? "Yatay" : "Dikey")")
+                     .font(.largeTitle)
+                     .bold()
+                     */
+                    
+                    VStack {
+                        Text("Ekranın Konumu: \(verticalSizeClass == .regular ? "Yatay" : "Dikey")")
+                            .font(.title)
+                            .bold()
+                        
+                        LottieView(animation: .named("orientation"))
+                            .looping()
+                            .frame(width: geo.size.width * 0.8, height: geo.size.height * 0.5)
+                    }
+                }
+                .navigationBarTitle("Device Orientation")
+            }
         }
-        .padding()
+        
     }
 }
 
